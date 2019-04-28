3d464a29e59cb10beef20059aa43753ba6bba983
Manisha <sadhwani.manisha@gmail.com>
2019-04-28 23:28:19 +0800

    String Accumulator : Initial Commit

---
+++ src/main/java/com/accumulator/StringAccumulator.java
@@ -0,0 +1,69 @@
+package com.accumulator;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringAccumulator {
+
+    private static final String ERROR_MESSAGE = "negatives not allowed:";
+
+    public int add(String numbers) {
+        int startIndex = numbers.indexOf("/");
+        String inputNum = numbers;
+        Splitter customSplitter;
+        if (startIndex == 0){   //check if the delimiter was provided as input then apply a different logic.
+            int firstNewLineIndex = numbers.indexOf("\n");
+            String delimiters = numbers.substring(2,firstNewLineIndex);
+            inputNum = numbers.substring(firstNewLineIndex,numbers.length());
+            customSplitter = customSplitter(delimiters);
+        }else{
+            //clients using default delimiters will bypass customSplitter logic.
+            customSplitter = defaultSplitter();
+        }
+
+        return sum(customSplitter.splitToList(inputNum));
+    }
+
+    private int sum(List<String> numList) {
+        int total=0;   //program asks to use int add(), int value may overflow if very large string is sent?
+        StringBuilder errorbuilder = new StringBuilder(ERROR_MESSAGE);
+        boolean hasError = false;
+        for(String value : numList){
+            int num = Integer.parseInt(value);
+            if (num <0){
+                hasError = true;
+                errorbuilder.append(" ").append(num);
+            }else if (num <= 1000){
+                total +=num;
+            }
+        }
+        if (hasError){
+            throw new IllegalArgumentException(errorbuilder.toString());
+        }
+
+        return total;
+    }
+
+    private Splitter customSplitter(String delimiters){
+        //assumption delimiters themselves will be delimited by "|". If the input doesn't follow the contract, this won't work.
+        //Input validations can be added here if required.
+        String escapedDelimiters = Splitter.on("|").trimResults()
+                .omitEmptyStrings()
+                .splitToList(delimiters)
+                .stream()
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+        return Splitter.onPattern(escapedDelimiters)
+                .trimResults()
+                .omitEmptyStrings();
+    }
+
+    private Splitter defaultSplitter(){
+        //if the usage is known, it can profiled and tested if adding this splitter as constant for multiple calls will save cost or not.
+        return Splitter.on(CharMatcher.anyOf(",\n")).trimResults().omitEmptyStrings();
+    }
+}
---
+++ src/test/java/com/accumulator/StringAccumulatorTest.java
@@ -0,0 +1,67 @@
+package com.accumulator;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class StringAccumulatorTest {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+
+    private StringAccumulator stringAccumulator;
+
+
+    @Before
+    public void setUp() {
+        stringAccumulator = new StringAccumulator();
+    }
+
+    @Test
+    public void addTwoNumbers_CommaDelim() {
+        assertEquals(5, stringAccumulator.add("1,4"));
+    }
+
+    @Test
+    public void addMoreNumbers_CommaDelim() {
+        assertEquals(20,stringAccumulator.add("1,2,3,4,0,10"));
+    }
+
+    @Test
+    public void addNumbers_SupportNewLineDelim() {
+        assertEquals(10,stringAccumulator.add("1,4\n5"));
+        assertEquals(10,stringAccumulator.add("4\n5,1\n,0"));
+    }
+
+    @Test
+    public void addNumbers_WithDelimiter_In_Input_String() {
+        assertEquals(8,stringAccumulator.add("//;\n1;7"));
+    }
+
+    @Test
+    public void addNumbers_Support_MultiChar_Delim_In_Input() {
+        assertEquals(5,stringAccumulator.add("//;*;|;;;\n1;;;4"));
+    }
+
+    @Test
+    public void add_Negative_Numbers_Throws_Exception() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("negatives not allowed: -1 -5");
+        stringAccumulator.add("-1,4,-5");
+    }
+
+    @Test
+    public void ignore_Number_Greater_Than_1000_While_OnAdd() {
+        assertEquals(105,stringAccumulator.add("100,1001\n5"));
+    }
+
+    @Test
+    public void boundary_test_add_1000() {
+        assertEquals(1009,stringAccumulator.add("//***\n1000***5***4"));
+    }
+
+}
